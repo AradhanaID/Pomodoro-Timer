c331e1000032cd34e0cb3bc454c916f60534e765
aradhanaid-wings <aradhanaindradev@gmail.com>
2023-04-05 14:09:19 +0700

    Using DaisyUI for Countdown

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,18 +2,28 @@ import { useContext, useMemo } from "react";
 import { TimerContext } from "../context/TimerContext";
 
 const Timer = () => {
-	const timerProvider = useContext(TimerContext);
-	if (!timerProvider) return null;
-	const { timer } = timerProvider;
+  const timerProvider = useContext(TimerContext);
+  if (!timerProvider) return null;
+  const { timer } = timerProvider;
 
-	const minutes = useMemo(() => Math.floor(timer / 60), [timer])
-	const seconds = useMemo(() => timer % 60, [timer])
-	
-	return (
-		<p className="text-8xl my-6">
-			{String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
-		</p>
-	);
+  const minutes = useMemo(() => Math.floor(timer / 60), [timer])
+  const seconds = useMemo(() => timer % 60, [timer])
+
+  const minutesStyles: Record<string, string> = { '--value': String(minutes).padStart(2, "0") }
+  const secondsStyles: Record<string, string> = { '--value': String(seconds).padStart(2, "0") }
+
+  return (
+    <p className="text-8xl my-6">
+      <span className="countdown">
+        <span style={minutesStyles}></span>
+      </span>
+      <span>:</span>
+      <span className="countdown">
+        <span style={secondsStyles}></span>
+      </span>
+      {/* {String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")} */}
+    </p>
+  );
 };
 
 export default Timer;
