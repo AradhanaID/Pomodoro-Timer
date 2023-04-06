b50ef65f12537e6b3ee0bd5e8fe147cc35b90153
aradhanaid-wings <aradhanaindradev@gmail.com>
2023-04-06 09:08:39 +0700

    Add PomodoroStatus

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { createContext, useContext, useState } from "react";
+import {useContext } from "react";
 import StartButton from "./components/StartButton";
 import ResetButton from "./components/ResetButton";
 
 import Timer from "./components/Timer";
-import { TimerContext, TimerContextProvider } from "./context/TimerContext";
+import { TimerContext } from "./context/TimerContext";
 import NextButton from "./components/NextButton";
+import PomodoroStatus from "./components/PomodoroStatus";
 
 function App() {
   const timer = useContext(TimerContext);
@@ -12,8 +13,9 @@ function App() {
 
   const { timerState } = timer;
   return (
-    <div className={`${timerState.current.value.backgroundColor} min-h-screen font-sans text-white flex flex-col gap-4 items-center pt-32 transition-colors`}>
-      <h1 className="font-semibold text-3xl">Pomodoro Timer</h1>
+    <div className={`${timerState.current.value.backgroundColor} min-h-screen font-sans text-white flex flex-col gap-4 items-center pt-24 transition-colors duration-200 ease-in-out`}>
+      <h1 className="font-semibold text-3xl mb-8">Pomodoro Timer</h1>
+      <PomodoroStatus />
       <Timer />
       <div className="flex items-center gap-6">
         <ResetButton />
---
+++ src/components/PomodoroStatus.tsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+import { TimerContext } from '../context/TimerContext';
+import { CupHot, GraphUpArrow, Headphones } from 'react-bootstrap-icons'
+
+const PomodoroStatus = () => {
+  const { timerState } = useContext(TimerContext)!;
+
+  switch (timerState.current.value.name) {
+    case 'Focus':
+      return (
+        <div className='flex gap-3 items-center'>
+          <GraphUpArrow width={36} height={36} />
+          <PomodoroStatusText text={timerState.current.value.name} />
+        </div>
+      )
+    case 'Short Break':
+      return (
+        <div className='flex gap-3 items-center'>
+          <CupHot width={36} height={36} />
+          <PomodoroStatusText text={timerState.current.value.name} />
+        </div>
+      )
+    case 'Long Break':
+      return (
+        <div className='flex gap-3 items-center'>
+          <Headphones width={36} height={36} />
+          <PomodoroStatusText text={timerState.current.value.name} />
+        </div>
+      )
+  }
+  return null;
+}
+
+const PomodoroStatusText = ({ text }: { text: string }) => (
+  <h2 className='text-4xl font-light'>
+    {text.toUpperCase()}
+  </h2>
+)
+export default PomodoroStatus
